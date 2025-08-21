600c77ff979824c467a92b5a899d683ee6a44f2d
VarunikaN <varunikaanaini@gmail.com>
2025-08-21 09:33:14 +0000

    edited dataset.py

--- datasets.py
+++ datasets.py
@@ -1,59 +1,108 @@
-# datasets.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 2022-12-13 09:54:12
+
+@author: XuWang
+
+"""
 
 import os
 import os.path
 import torch.utils.data as data
 from PIL import Image
+import cv2
+from PIL import Image
 import numpy as np
 
+
+
+import os
+from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset
+from torchvision import transforms
+import custom_transforms as tr
+from torchvision.datasets.folder import is_image_file
+
+
 def make_dataset(root):
-    # Logic to find image and label pairs
+    # image_path = os.path.join(root, 'Image')
+    mask_path = os.path.join(root, 'GT')
+    image_path = root
     mask_path = root + '_labels'
-    img_list = [os.path.splitext(f)[0] for f in os.listdir(root) if f.endswith('.jpg')]
-    return [(os.path.join(root, img_name + '.jpg'), os.path.join(mask_path, img_name + '.png')) for img_name in img_list]
+    
+    #Images' format is jpg, GTs' format is png
+    img_list = [os.path.splitext(f)[0] for f in os.listdir(image_path) if f.endswith('.jpg')]
+    return [(os.path.join(image_path, img_name + '.jpg'), os.path.join(mask_path, img_name + '.png')) for img_name in img_list]
+
 
 class ImageFolder(data.Dataset):
-    def __init__(self, root, joint_transform=None, transform=None, target_transform=None):
+    # image and gt should be in the same folder and have same filename except extended name (jpg and png respectively)
+    def __init__(self, root, joint_transform=None, transform=None, target_transform=None, split='train'):
         self.root = root
         self.imgs = make_dataset(root)
         self.joint_transform = joint_transform
         self.transform = transform
         self.target_transform = target_transform
-
-    def convert_label(self, label_img):
-        """
-        Converts a multi-class label image into a binary (0/1) mask.
-        This is the critical missing step.
-        """
-        label_array = np.array(label_img, dtype=np.uint8)
-        # Create a new array initialized to 0 (background)
-        binary_mask = np.zeros_like(label_array)
-        # Set all pixels with a value > 0 to 1 (foreground)
-        binary_mask[label_array > 0] = 1
-        # Convert back to a PIL Image
-        return Image.fromarray(binary_mask, mode='L')
+        self.split = split
+        self.label_mapping = {-1: -1, 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6:6, 7:7, 8:8, 9:9, 10:10,
+                              11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16:16, 17:17, 18:18, 19:19, 20:20,
+                              21: 21, 22: 22, 23: 23, 24: 24, 25: 25, 26:26, 27:27, 28:28, 29:29, 30:30}
 
     def __getitem__(self, index):
         img_path, gt_path = self.imgs[index]
         img = Image.open(img_path).convert('RGB')
+        target = Image.open(gt_path)
+        label = self.convert_label(target)
         
-        # Load the original ground truth image
-        original_target = Image.open(gt_path)
+        # print(gt_path)
+        # print(target.size)
+        # print(label.size)
+
+        # if self.joint_transform is not None:
+        #     img, label = self.joint_transform(img, label)
         
-        # Convert it to a binary mask using the essential function
-        target = self.convert_label(original_target)
+        sample = {'image': img, 'label': label}
+        if self.split == "train":
+            return self.transform_tr(sample) 
+        else:
+            sample = self.transform_val(sample)
+            sample['name'] = self.imgs[index]
+            return sample
+        
+    
+    
+    def convert_label(self, label):
+        label_rgb = np.array(label)
+        label_index = np.full(label_rgb.shape[:2], 0, dtype='uint8')
+        
+        for k in range(1,30):
+            label_index[label_rgb == k] = 1
 
-        if self.joint_transform is not None:
-            img, target = self.joint_transform(img, target)
+        label_index = Image.fromarray(label_index, mode='P')
 
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-            
-        # The target is now a (1, H, W) tensor of 0s and 1s. Squeeze it to (H, W).
-        return {'image': img, 'label': target.squeeze(0)}
+        return label_index
 
     def __len__(self):
         return len(self.imgs)
+    
+    
+    def transform_tr(self, sample):
+        composed_transforms = transforms.Compose([
+            tr.RandomHorizontalFlip(),
+            tr.FixedResize(576,896),
+            # tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
+            tr.RandomCrop((576,576)),
+            tr.RandomGaussianBlur(),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+        return composed_transforms(sample)
+ 
+    def transform_val(self, sample):
+        composed_transforms = transforms.Compose([
+            # tr.FixScaleCrop(crop_size=self.args.crop_size),
+            tr.FixedResize(576,896),
+            tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+            tr.ToTensor()])
+        return composed_transforms(sample)
