119a7780b46f21d4d5368a60e2e85a2fdab539f9
VarunikaN <varunikaanaini@gmail.com>
2025-08-22 04:47:27 +0000

    added checkpointing to drive

--- config.py
+++ config.py
@@ -4,9 +4,12 @@ import os
 
 DATA_ROOT = './data'
 
-CKPT_ROOT = './ckpt'
+CKPT_ROOT = '/content/drive/MyDrive/araa/ARAA-Net/ckpt'
 
 DATASET_NAME = 'TSRS_RSNA-Epiphysis'
+# DATASET_NAME = 'TSRS_RSNA-Articular-Surface'
+
+
 
 dataset_path = os.path.join(DATA_ROOT, DATASET_NAME)
 cod_training_root = os.path.join(dataset_path, 'train')
--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='Train ARAA-Net with original logic')
     parser.add_argument('--backbone', type=str, default='resnet50', choices=['resnet50', 'resnet101', 'vgg16'], help='Choose the backbone model')
     parser.add_argument('--epochs', type=int, default=100, help='Number of training epochs')
-    parser.add_argument('--batch-size', type=int, default=5, help='Batch size for training')
+    parser.add_argument('--batch-size', type=int, default=5, help='Batch size for training') #change to 3 for vgg
     parser.add_argument('--lr', type=float, default=1e-3, help='Base learning rate')
     parser.add_argument('--lr-decay', type=float, default=0.9, help='Exponent for polynomial LR decay')
     parser.add_argument('--weight-decay', type=float, default=5e-4, help='Weight decay')
