5f4c08b71a0e3b195e1461465d5b8865e148124e
VarunikaN <varunikaanaini@gmail.com>
2025-09-02 05:59:03 +0000

    epochs updated

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ import torch.nn.functional as F
 def get_args():
     parser = argparse.ArgumentParser(description='Train ARAA-Net with multi-backbone support')
     parser.add_argument('--backbone', type=str, default='resnet50', choices=['resnet50', 'resnet101', 'vgg16', 'inception_v3'], help='Choose backbone')
-    parser.add_argument('--epoch-num', type=int, default=1000, help='Number of training epochs')
-    parser.add_argument('--train-batch-size', type=int, default=2, help='Batch size for training') 
+    parser.add_argument('--epoch-num', type=int, default=100, help='Number of training epochs')
+    parser.add_argument('--train-batch-size', type=int, default=5, help='Batch size for training') 
     parser.add_argument('--lr', type=float, default=1e-3, help='Base learning rate')
     parser.add_argument('--weight-decay', type=float, default=5e-4, help='Weight decay')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for SGD optimizer')
