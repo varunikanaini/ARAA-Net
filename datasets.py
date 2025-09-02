b330dd43177b0d2bc3f4b3db816f63d094f4e519
VarunikaN <varunikaanaini@gmail.com>
2025-09-02 04:15:25 +0000

    updated datsets.py and test.py

--- datasets.py
+++ datasets.py
@@ -37,9 +37,79 @@ def make_dataset(root):
     return [(os.path.join(image_path, img_name + '.jpg'), os.path.join(mask_path, img_name + '.png')) for img_name in img_list]
 
 
+# class ImageFolder(data.Dataset):
+#     # image and gt should be in the same folder and have same filename except extended name (jpg and png respectively)
+#     def __init__(self, root, joint_transform=None, transform=None, target_transform=None, split='train'):
+#         self.root = root
+#         self.imgs = make_dataset(root)
+#         self.joint_transform = joint_transform
+#         self.transform = transform
+#         self.target_transform = target_transform
+#         self.split = split
+#         self.label_mapping = {-1: -1, 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6:6, 7:7, 8:8, 9:9, 10:10,
+#                               11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16:16, 17:17, 18:18, 19:19, 20:20,
+#                               21: 21, 22: 22, 23: 23, 24: 24, 25: 25, 26:26, 27:27, 28:28, 29:29, 30:30}
+
+#     def __getitem__(self, index):
+#         img_path, gt_path = self.imgs[index]
+#         img = Image.open(img_path).convert('RGB')
+#         target = Image.open(gt_path)
+#         label = self.convert_label(target)
+        
+#         # print(gt_path)
+#         # print(target.size)
+#         # print(label.size)
+
+#         # if self.joint_transform is not None:
+#         #     img, label = self.joint_transform(img, label)
+        
+#         sample = {'image': img, 'label': label}
+#         if self.split == "train":
+#             return self.transform_tr(sample) 
+#         else:
+#             sample = self.transform_val(sample)
+#             sample['name'] = self.imgs[index]
+#             return sample
+        
+    
+    
+#     def convert_label(self, label):
+#         label_rgb = np.array(label)
+#         label_index = np.full(label_rgb.shape[:2], 0, dtype='uint8')
+        
+#         for k in range(1,30):
+#             label_index[label_rgb == k] = 1
+
+#         label_index = Image.fromarray(label_index, mode='P')
+
+#         return label_index
+
+#     def __len__(self):
+#         return len(self.imgs)
+    
+    
+#     def transform_tr(self, sample):
+#         composed_transforms = transforms.Compose([
+#             tr.RandomHorizontalFlip(),
+#             tr.FixedResize(576,896),
+#             # tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
+#             tr.RandomCrop((576,576)),
+#             tr.RandomGaussianBlur(),
+#             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+#             tr.ToTensor()])
+#         return composed_transforms(sample)
+ 
+#     def transform_val(self, sample):
+#         composed_transforms = transforms.Compose([
+#             # tr.FixScaleCrop(crop_size=self.args.crop_size),
+#             tr.FixedResize(576,896),
+#             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+#             tr.ToTensor()])
+#         return composed_transforms(sample)
+    
+
 class ImageFolder(data.Dataset):
-    # image and gt should be in the same folder and have same filename except extended name (jpg and png respectively)
-    def __init__(self, root, joint_transform=None, transform=None, target_transform=None, split='train'):
+    def __init__(self, root, joint_transform=None, transform=None, target_transform=None, split='train', scale_h=896, scale_w=576, crop_size=576):
         self.root = root
         self.imgs = make_dataset(root)
         self.joint_transform = joint_transform
@@ -49,19 +119,16 @@ class ImageFolder(data.Dataset):
         self.label_mapping = {-1: -1, 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6:6, 7:7, 8:8, 9:9, 10:10,
                               11: 11, 12: 12, 13: 13, 14: 14, 15: 15, 16:16, 17:17, 18:18, 19:19, 20:20,
                               21: 21, 22: 22, 23: 23, 24: 24, 25: 25, 26:26, 27:27, 28:28, 29:29, 30:30}
+        # NEW: Store dynamic sizes
+        self.scale_h = scale_h
+        self.scale_w = scale_w
+        self.crop_size = crop_size
 
     def __getitem__(self, index):
         img_path, gt_path = self.imgs[index]
         img = Image.open(img_path).convert('RGB')
         target = Image.open(gt_path)
         label = self.convert_label(target)
-        
-        # print(gt_path)
-        # print(target.size)
-        # print(label.size)
-
-        # if self.joint_transform is not None:
-        #     img, label = self.joint_transform(img, label)
         
         sample = {'image': img, 'label': label}
         if self.split == "train":
@@ -71,8 +138,6 @@ class ImageFolder(data.Dataset):
             sample['name'] = self.imgs[index]
             return sample
         
-    
-    
     def convert_label(self, label):
         label_rgb = np.array(label)
         label_index = np.full(label_rgb.shape[:2], 0, dtype='uint8')
@@ -87,22 +152,21 @@ class ImageFolder(data.Dataset):
     def __len__(self):
         return len(self.imgs)
     
-    
+    # MODIFIED: Use self.scale_h, self.scale_w, self.crop_size
     def transform_tr(self, sample):
         composed_transforms = transforms.Compose([
             tr.RandomHorizontalFlip(),
-            tr.FixedResize(576,896),
-            # tr.RandomScaleCrop(base_size=self.args.base_size, crop_size=self.args.crop_size),
-            tr.RandomCrop((576,576)),
+            tr.FixedResize(self.scale_h, self.scale_w), # Dynamic resize
+            tr.RandomCrop((self.crop_size, self.crop_size)), # Dynamic crop
             tr.RandomGaussianBlur(),
             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             tr.ToTensor()])
         return composed_transforms(sample)
  
+    # MODIFIED: Use self.scale_h, self.scale_w
     def transform_val(self, sample):
         composed_transforms = transforms.Compose([
-            # tr.FixScaleCrop(crop_size=self.args.crop_size),
-            tr.FixedResize(576,896),
+            tr.FixedResize(self.scale_h, self.scale_w), # Dynamic resize
             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             tr.ToTensor()])
         return composed_transforms(sample)
