84774b08476366e115fa2b4fb03eeddc030f3c77
varunikanaini <varunika.naini@dhyeyo.ai>
2025-09-23 17:30:34 +0000

    nn import error F error

--- test_lasa_vgg.py
+++ test_lasa_vgg.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F  
 import datetime
 import argparse
 import logging
